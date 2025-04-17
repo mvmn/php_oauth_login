3a250e21bacbbffab5e6e3bbca48bdeec031dfff
Mykola Makhin <mvmn.inbox@gmail.com>
2025-04-17 15:54:13 +0300

    Added Google link to index. Added other envs env php files to gitignore

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ session_start();
 <html>
 <body>
 Login with:<br/>
-<a href="discord_oauth.php" >Discord</a>
+<a href="discord_oauth.php">Discord</a>
+<a href="google_oauth.php">Google</a>
 </body>
 </html>
